8fc709046c55989e3d6e41d41da2725d9a6e80c8
James O'Doherty <james@theodohertyfamily.com>
2023-11-16 22:30:16 -0500

    Fix float colors

--- lua/unixdark.lua
+++ lua/unixdark.lua
@@ -4,3 +4,8 @@ vim.opt.termguicolors = true
 vim.cmd [[ hi ColorColumn guibg=#181818 ]]
 vim.cmd [[ hi LineNr guibg=#333333 guifg=#888888 ]]
 vim.cmd [[ hi SignColumn guibg=#252525 ]]
+
+vim.api.nvim_set_hl(0, 'FloatBorder', {bg='#3B4252', fg='#5E81AC'})
+vim.api.nvim_set_hl(0, 'NormalFloat', {bg='#3B4252'})
+vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg='#3B4252'})
+vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg='#3B4252'})
