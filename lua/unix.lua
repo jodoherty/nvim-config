2a8ae1ae924344d8b4a434fd225053cb962a1048
James O'Doherty <james@theodohertyfamily.com>
2024-05-05 14:14:22 -0400

    Feeling blue

--- lua/unix.lua
+++ lua/unix.lua
@@ -1,5 +1,7 @@
 vim.opt.bg = "dark"
-vim.cmd [[ colorscheme darkblue ]]
-vim.cmd [[ hi LineNr guibg=#333333 guifg=#888888 ]]
-vim.cmd [[ hi SignColumn guibg=#252525 ]]
+vim.cmd [[ colorscheme blue ]]
+vim.cmd [[ hi LineNr guibg=#101060 ]]
+vim.cmd [[ hi Whitespace guifg=#2020c0 ]]
+vim.cmd [[ hi SpecialKey guifg=#2020c0 ]]
+
 
--- lua/windows.lua
+++ lua/windows.lua
@@ -1,7 +1,8 @@
 vim.opt.fileformats = "unix,dos"
 vim.opt.fileencodings = "utf-8,ucs-bom,default,latin1"
 vim.opt.bg = "dark"
-vim.cmd[[colorscheme darkblue]]
-vim.cmd[[hi LineNr guibg=#151555]]
-vim.cmd[[hi SignColumn guibg=#151555]]
+vim.cmd [[ colorscheme blue ]]
+vim.cmd [[ hi LineNr guibg=#101060 ]]
+vim.cmd [[ hi Whitespace guifg=#2020c0 ]]
+vim.cmd [[ hi SpecialKey guifg=#2020c0 ]]
 
