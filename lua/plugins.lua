54e764ee95603b4072392d850f3f2c9016f059ce
James O'Doherty <james@theodohertyfamily.com>
2025-03-16 06:58:17 -0400

    Disable neotree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,16 +2,16 @@ return {
   { "Olical/conjure" },
   {'sbdchd/neoformat'},
   {'neovim/nvim-lspconfig'},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  },
+  --{
+  --  "nvim-neo-tree/neo-tree.nvim",
+  --  branch = "v3.x",
+  --  dependencies = {
+  --    "nvim-lua/plenary.nvim",
+  --    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+  --    "MunifTanjim/nui.nvim",
+  --    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  --  }
+  --},
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
